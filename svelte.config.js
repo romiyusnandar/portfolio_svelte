2e1d1acb189ba834ddfb65c112997063e4e1bbde
romiyusnandar <yusromi04@gmail.com>
2025-01-03 21:08:00 +0700

    Changes for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    }
   },
   preprocess: vitePreprocess()
 };
