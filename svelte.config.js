f845a33b1db7a38c0297f7fe598f5adcfde9bbe1
romiyusnandar <yusromi04@gmail.com>
2025-01-03 18:12:54 +0700

    remove node

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
   kit: {
-    adapter: adapter({
-      runtime: 'nodejs20.x',
-    })
+    adapter: adapter()
   },
   preprocess: vitePreprocess()
 };
