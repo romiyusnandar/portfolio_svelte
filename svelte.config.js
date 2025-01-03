aafc80a690cf30a02d7e490e5558223ea202c450
romiyusnandar <yusromi04@gmail.com>
2025-01-03 21:24:24 +0700

    Add this too

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: '200.html'
+    }),
     paths: {
       base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
     }
